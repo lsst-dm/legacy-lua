6ce187e51a2561d3f2e3ece113599243679b72ad
Mario Juric <mjuric@lsst.org>
2014-12-07 23:40:25 -0800

    OS X compatibility fixes
      * -i option on OSX version of sed requires an argument
      * correctly set PLAT=macosx if Darwin is detected

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -5,9 +5,19 @@ export LANG=C
 
 config()
 {
-    sed -i "s/PLAT= none/PLAT= linux/" Makefile &&
-    sed -i "s,INSTALL_TOP= /usr/local,INSTALL_TOP= ${PREFIX}," Makefile &&
-    sed -i "s,CFLAGS= -O2,CFLAGS= -I${PREFIX}/include -fPIC -O2," src/Makefile &&
-    sed -i "s,LIBS= -lm,LIBS= -L${PREFIX}/lib -lm," src/Makefile &&
-    sed -i "s,#define LUA_ROOT\t\"/usr/local/\",#define LUA_ROOT\t\"${PREFIX}/\"," src/luaconf.h
+	# Detect platform
+	case "$(uname)" in
+	Linux)
+		PLAT="linux" ;;
+	Darwin)
+		PLAT="macosx" ;;
+	*)
+		die "Unsupported platform $(uname); aborting build."
+	esac
+
+	sed -i '.prev' "s/PLAT= none/PLAT= ${PLAT}/" Makefile &&
+	sed -i '.prev' "s,INSTALL_TOP= /usr/local,INSTALL_TOP= ${PREFIX}," Makefile &&
+	sed -i '.prev' "s,CFLAGS= -O2,CFLAGS= -I${PREFIX}/include -fPIC -O2," src/Makefile &&
+	sed -i '.prev' "s,LIBS= -lm,LIBS= -L${PREFIX}/lib -lm," src/Makefile &&
+	sed -i '.prev' "s,#define LUA_ROOT\t\"/usr/local/\",#define LUA_ROOT\t\"${PREFIX}/\"," src/luaconf.h
 }
